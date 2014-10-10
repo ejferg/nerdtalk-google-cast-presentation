c70f1d111c0ea53a6f76c664e33c1e2cb6b3e43c
Earl Ferguson <earl@ullisen.com>
2014-10-10 05:55:26 -0400

    Receiver compelete

--- presentation/js/cast.receiver.js
+++ presentation/js/cast.receiver.js
@@ -14,7 +14,13 @@
 
       cast.receiver.logger.setLevelValue(0);
 
+      // Initializes the system manager so we can communicate with the platform.
+      // This class is used to send/receive system messages/events.
       manager = cast.receiver.CastReceiverManager.getInstance();
+
+      // Handles cast messages for a specific namespace.
+      // Applications should never create a CastMessageBus
+      // Provides a channel for a specific namespace (for any sender).
       messageBus = manager.getCastMessageBus(app.AppCast.NAMESPACE);
 
       addEventListeners();
@@ -24,6 +30,8 @@
     // Utility Methods
 
     var start = function() {
+
+      // Initializes the system manager.
       manager.start({statusText: "Application is starting"});
     }
 
@@ -42,6 +50,7 @@
 
       logger.info(e);
 
+      // Sends a message to a specific sender.
       messageBus.send(e.senderId, e.data);
 
       if(messageHandler) {
@@ -57,6 +66,7 @@
       manager.onReady = _.bind(onReady, this);
       manager.onSenderConnected = _.bind(onSenderConnected, this);
 
+      // Event handler for cast.receiver.CastMessageBus message event.
       messageBus.onMessage = _.bind(onMessage, this);
 
     };
